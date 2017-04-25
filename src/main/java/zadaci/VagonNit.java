8f55010ee79acf7e9af1823476936c86c765a2f4
borisbokan <borisbokan@gmail.com>
2017-04-25 21:03:13 +0200

    Uradjen zadatak niti (ne funkcionise bas .() fixes #10

---
+++ src/main/java/zadaci/VagonNit.java
@@ -0,0 +1,100 @@
+package zadaci;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.QueryBuilder;
+import model.Vagon;
+import model.Voz;
+
+import javax.management.Query;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by androiddevelopment on 25.4.17..
+ */
+public class VagonNit extends Thread {
+
+
+    private Vagon vagon;
+    private String oznaka;
+
+
+    public VagonNit(String _oznaka,Vagon _vagon){
+        this.oznaka=_oznaka;
+        this.vagon=_vagon;
+
+    }
+
+
+    private void utovar(){
+
+
+        synchronized (vagon){
+            Poruka.naslov("Krece utovar vagona " + this.vagon.getOznaka());
+            do{
+                Poruka.text("tovari u vagon......>>");
+                Random ra=new Random();
+                long period=ra.nextInt(2) * 1000;
+                this.vagon.setTeret(this.vagon.getTeret() + 1);
+                try {
+                    sleep(period);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Poruka.naslov("zavrsen utovar vagona " + this.vagon.getOznaka());
+
+            }while (this.vagon.getTeret() < this.vagon.getNosivost());
+
+            Poruka.naslov("....Kraj...");
+        }
+
+    }
+
+
+    @Override
+    public void run(){
+            utovar();
+
+    }
+
+
+ //*********************************************TEST****************************
+    public static void main(String[] args){
+
+        myDB db = new myDB();
+
+        try {
+
+            Dao<Vagon, Integer> DAOvagon = DaoManager.createDao(db.getKonekcija(), Vagon.class);
+            Dao<Voz, Integer> DAOvoz = DaoManager.createDao(db.getKonekcija(), Voz.class);
+
+          /*  QueryBuilder<Vagon,Integer> upit=DAOvagon.queryBuilder();
+            QueryBuilder<Voz,Integer> upitVoz=DAOvoz.queryBuilder();
+            List<Vagon> vagoni=upit.leftJoin(upitVoz).where().eq(Voz.POLJE_OZNAKA,"Voz1").query();*/
+
+            QueryBuilder<Vagon,Integer> upit=DAOvagon.queryBuilder();
+            List<Vagon> vagoni=upit.query();
+
+            for (Vagon vagon : vagoni ) {
+                if(vagon.getVoz().getNaziv().contentEquals("Voz1")){
+                    VagonNit vag=new VagonNit(vagon.getOznaka().toString(),vagon);
+                    vag.start();
+
+                    try {
+                        vag.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
